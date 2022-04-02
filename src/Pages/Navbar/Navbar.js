fa70666cf79b4df9b9c546bc4d2ea12ff60a2b2a
arfin-web <arfinpriom@gmail.com>
2022-04-02 15:03:47 +0600

    done

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <>
-
+            <nav className="navbar navbar-expand-lg sticky-top main-nav">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand" to="/"><h4 className='logo fw-bold'><span className='logo-color'>Pay</span>Cube</h4></NavLink>
+                    <div className='ms-auto'>
+                        <button className='btn btn-bg fw-bold'>Login / Register</button>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 };
