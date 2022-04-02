d37a6d45abe6b1303e655791eb9f0710d4bd1a86
Shpon Roy <shiponroy509@gmail.com>
2022-04-02 13:54:48 +0600

    commit footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Footer/>
     </div>
   );
 }
