6e98d2451c7e625d8bbbcc1793872844c42218ea
Shpon Roy <shiponroy509@gmail.com>
2022-04-19 14:25:27 +0600

    rates api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import AllChart from './components/Chart/AllChart/AllChart';
+import UpdateRates from './components/UpdateRates/UpdateRates';
 
 function App() {
   return (
     <div>
+      <UpdateRates></UpdateRates>
       <AllChart/>
       <Footer/>
     </div>
